7cb256f57c595adaedc82af70f445c992575ed54
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-23 13:01:19 +0200

    Added support to fetch the book by its ID

---
+++ src/main/java/com/odeyalo/grpc/books/api/grpc/DefaultGrpcBookService.java
@@ -0,0 +1,41 @@
+package com.odeyalo.grpc.books.api.grpc;
+
+import com.odeyalo.grpc.books.client.book.Book;
+import com.odeyalo.grpc.books.client.book.Book.BookDto;
+import com.odeyalo.grpc.books.client.book.BookServiceGrpc;
+import com.odeyalo.grpc.books.entity.BookEntity;
+import com.odeyalo.grpc.books.repository.BookRepository;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.UUID;
+
+@GrpcService
+public final class DefaultGrpcBookService extends BookServiceGrpc.BookServiceImplBase {
+    private final BookRepository bookRepository;
+
+    public DefaultGrpcBookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void fetchBook(@NotNull final Book.FetchBookRequest request,
+                          @NotNull final StreamObserver<BookDto> responseObserver) {
+
+        bookRepository.findBookById(UUID.fromString(request.getBookId()))
+                .map(this::toBookDto)
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(responseObserver::onNext,
+                        responseObserver::onError,
+                        responseObserver::onCompleted);
+
+    }
+
+    private BookDto toBookDto(BookEntity entity) {
+        return BookDto.newBuilder()
+                .setId(entity.getId().toString())
+                .build();
+    }
+}
---
+++ src/test/java/com/odeyalo/grpc/books/api/grpc/DefaultGrpcBookServiceTest.java
@@ -0,0 +1,65 @@
+package com.odeyalo.grpc.books.api.grpc;
+
+import com.odeyalo.grpc.books.client.book.Book.BookDto;
+import com.odeyalo.grpc.books.client.book.Book.FetchBookRequest;
+import com.odeyalo.grpc.books.entity.BookEntity;
+import com.odeyalo.grpc.books.repository.BookRepository;
+import com.odeyalo.grpc.books.repository.InMemoryBookRepository;
+import io.grpc.internal.testing.StreamRecorder;
+import org.junit.jupiter.api.Test;
+import testing.faker.BookEntityFaker;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultGrpcBookServiceTest {
+
+    public static final BookEntity EXISTING_BOOK = BookEntityFaker.create().get();
+
+    @Test
+    void shouldFetchBookByItsId() throws Exception {
+        final DefaultGrpcBookService testable = TestableBuilder
+                .builder()
+                .withBooks(EXISTING_BOOK)
+                .build();
+
+        String bookId = EXISTING_BOOK.getId().toString();
+
+        FetchBookRequest fetchBookRequest = FetchBookRequest.newBuilder()
+                .setBookId(bookId)
+                .build();
+
+        BookDto found = fetchBook(testable, fetchBookRequest);
+
+        assertThat(found.getId()).isEqualTo(bookId);
+    }
+
+    private static BookDto fetchBook(DefaultGrpcBookService testable, FetchBookRequest fetchBookRequest) throws Exception {
+        StreamRecorder<BookDto> recorder = StreamRecorder.create();
+
+        testable.fetchBook(fetchBookRequest, recorder);
+
+        recorder.awaitCompletion(5, TimeUnit.SECONDS);
+
+        return recorder.firstValue().get();
+    }
+
+
+    static class TestableBuilder {
+        private BookRepository bookRepository = new InMemoryBookRepository();
+
+        public static TestableBuilder builder() {
+            return new TestableBuilder();
+        }
+
+        public TestableBuilder withBooks(BookEntity... books) {
+            bookRepository = new InMemoryBookRepository(books);
+            return this;
+        }
+
+        public DefaultGrpcBookService build() {
+            return new DefaultGrpcBookService(bookRepository);
+        }
+    }
+}
