03a31e8dced199bacdde6e04547c2e1716423a31
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-23 12:57:02 +0200

    Represent the Book in the system

---
+++ src/main/java/com/odeyalo/grpc/books/model/Book.java
@@ -0,0 +1,21 @@
+package com.odeyalo.grpc.books.model;
+
+import lombok.Builder;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+@Value
+@Builder
+public class Book {
+    @NotNull
+    UUID id;
+    @NotNull
+    String name;
+    @NotNull
+    String author;
+    @NotNull
+    String isbn;
+    int quantity;
+}
