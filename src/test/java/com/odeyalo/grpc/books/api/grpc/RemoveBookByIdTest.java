d719afde53eb427a9ca002dd92b949922233a9a0
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-25 16:59:21 +0200

    Added check if book is actually deleted from system

--- src/test/java/com/odeyalo/grpc/books/api/grpc/RemoveBookByIdTest.java
+++ src/test/java/com/odeyalo/grpc/books/api/grpc/RemoveBookByIdTest.java
@@ -2,6 +2,7 @@ package com.odeyalo.grpc.books.api.grpc;
 
 import com.odeyalo.grpc.books.api.grpc.Book.DeletionStatus;
 import com.odeyalo.grpc.books.entity.BookEntity;
+import com.odeyalo.grpc.books.exception.BookNotFoundException;
 import com.odeyalo.grpc.books.exception.RequestValidationException;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
@@ -13,6 +14,7 @@ import static testing.factory.DefaultGrpcReactiveBookServiceTestableBuilder.test
 
 class RemoveBookByIdTest extends AbstractBookClientTest {
     private static final String EXISTING_BOOK_ID = UUID.randomUUID().toString();
+
     private static final BookEntity EXISTING_BOOK = BookEntityFaker.create()
             .setId(UUID.fromString(EXISTING_BOOK_ID))
             .get();
@@ -22,6 +24,10 @@ class RemoveBookByIdTest extends AbstractBookClientTest {
 
     private static final Book.DeleteBookRequest MALFORMED_DELETE_EXISTING_BOOK_REQUEST = Book.DeleteBookRequest.newBuilder()
             .setBookId("123")
+            .build();
+
+    private static final Book.FetchBookRequest FETCH_EXISTING_BOOK_REQUEST = Book.FetchBookRequest.newBuilder()
+            .setBookId(EXISTING_BOOK_ID)
             .build();
 
     @Test
@@ -58,5 +64,22 @@ class RemoveBookByIdTest extends AbstractBookClientTest {
                 .as(StepVerifier::create)
                 .expectNext(DeletionStatus.SUCCESS)
                 .verifyComplete();
+    }
+
+    @Test
+    void shouldActuallyDeleteTheBookFromService() {
+        DefaultReactiveBookService testable = testableBuilder()
+                .withBooks(EXISTING_BOOK)
+                .build();
+
+        testable.removeBook(DELETE_EXISTING_BOOK_REQUEST)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        testable.fetchBook(FETCH_EXISTING_BOOK_REQUEST)
+                .as(StepVerifier::create)
+                .expectError(BookNotFoundException.class)
+                .verify();
     }
 }
